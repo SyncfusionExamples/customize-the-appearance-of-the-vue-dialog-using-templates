6e583e41a7223666cce17e565f1e80fdd8a9a2f1
Niluba <139118395+Niluba@users.noreply.github.com>
2024-08-14 12:14:44 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# customize-the-appearance-of-the-vue-dialog-using-templates
+# Customize the Appearance of the Vue Dialog Using Templates
+
 A quick-start project that helps you to customize the Syncfusion Vue Dialog component appearance using templates in a Vue application. This project contains code to customize a header, content and footer of the dialog.
+ 
+Refer to the following documentation to learn about the Vue Dialog component: 
+https://ej2.syncfusion.com/vue/documentation/dialog/template
+
+Check out this online example of the Vue Dialog component:
+https://ej2.syncfusion.com/vue/demos/#/bootstrap5/dialog/template.html
+
+## Project prerequisites
+Make sure that you have the compatible versions of [Visual Studio Code](https://code.visualstudio.com/download ) and [NodeJS](https://nodejs.org/en/download) or later version in your machine before starting to work on this project.
+
+## How to run this application
+To run this application, you need to first clone the `customize-the-appearance-of-the-vue-dialog-using-templates` repository and then open it in Visual Studio Code. Now, simply build and run your project to view the output.
---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<script setup>
+import {DialogComponent as EjsDialog} from '@syncfusion/ej2-vue-popups';
+
+</script>
+
+<template>
+
+<ejs-dialog height="200px" width="370px" :header="'headerTemplate'" :content="'contentTemplate'"
+:footerTemplate="'footerTemplate'"
+showCloseIcon="true">
+<template v-slot:headerTemplate>
+  <div class="header">
+    <img class="image" src="\logo.png" />   
+    <span id="template"> Syncfusion </span>
+  </div>
+</template>
+<template v-slot:contentTemplate>
+    <div class="content">  
+       <span class="dialogText">Want to be alerted when our videos are published? Enter your mail address to be the first notified!</span>
+      </div>
+ </template>
+<template v-slot:footerTemplate>
+  <div class="footer">
+  <input id='textbox' class='e-input' placeholder='Enter your mail id'/>
+  <button class='e-info e-btn'>SUBSCRIBE</button>
+</div>
+</template>
+</ejs-dialog>
+</template>
+
+<style scoped>
+@import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-popups/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-buttons/styles/material.css";
+ #template {
+   font-weight: bold;
+   font-size:20px;
+   margin:6px; 
+}
+.image{
+        width: 40px;
+        height: 35px;
+    } 
+    .header{
+  display:inline-flex;
+}
+
+.content {
+  font-family: serif;
+  font-size: 16px;
+}
+#textbox {
+    float: left;
+    width: 65%;
+    padding:2px;
+}
+.footer{
+  padding: 10px;
+}
+</style>
+
